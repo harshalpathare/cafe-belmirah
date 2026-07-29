e330aef6b07be5129df2ce77ed9218b1f2e9e681
harshal2 <pathareharshal68@gmail.com>
2026-07-29 15:16:36 +0530

    Support MySQL DATABASE_URL

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,15 +5,16 @@ dotenv.config();
 
 const dialect = (process.env.DB_DIALECT as any) || 'sqlite';
 
+const isPostgres = process.env.DATABASE_URL?.startsWith('postgres');
+
 export const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      dialectOptions: {
+      dialectOptions: isPostgres ? {
         ssl: {
           require: true,
           rejectUnauthorized: false
         }
-      },
+      } : undefined,
       logging: false,
     })
   : new Sequelize({
